cb96c70b1cfe5e2299ccf093fca48a0719f40343
hadiheidariazar <hadiha010101@gmail.com>
2024-09-03 11:05:11 +0330

    added styles default

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import "./custom.css"
+import "./styles/css/helpers.css"
+import "bootstrap/dist/css/bootstrap.rtl.min.css"
 
-createRoot(document.getElementById('root')).render(
-  <App />,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
