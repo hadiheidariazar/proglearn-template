7c3aecb38796861008bad6b57dded4a4a4c44806
hadiheidariazar <hadiha010101@gmail.com>
2024-09-03 11:11:30 +0330

    added Home page at routes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
 
 export default function App() {
+
+  const router = useRoutes(routes)
+
   return (
-    <div>App</div>
+    <>
+      {router}
+    </>
   )
 }
