3056d58a42dba193c01c501693299d69f6bd7577
hadiheidariazar <hadiha010101@gmail.com>
2024-09-16 19:09:07 +0330

    coding static items user panel

---
+++ src/Pages/UserPanel/Index.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Sidebar from '../../Components/UserPanel/Sidebar/Sidebar'
+import { Outlet } from 'react-router-dom'
+import OffcanvasMenu from '../../Components/UserPanel/OffcanvasMenu/OffcanvasMenu'
+import { AiFillHome } from 'react-icons/ai'
+import "./Index.css"
+
+export default function Index() {
+    return (
+        <>
+            <div className="container-fluid bg-white">
+                <div className="content">
+                    <div className="row">
+                        <div className="col-0 col-lg-3 d-none d-lg-grid bg-white ps-5">
+                            <div className="sidebar bg-white">
+                                <div className="sidebar-header-img mt-5">
+                                    <a href="/">
+                                        <img src="/images/logo/Logo.png" alt="ProgLearn Logo" className='img-fluid' />
+                                    </a>
+                                </div>
+                                <Sidebar isAdmin={true} />
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-9 sidebar-left">
+                            <div className="content-sidebar bg-secondary-500 p-4 p-md-5">
+                                <div className="topbar">
+                                    <div className="row mb-5">
+                                        <div className="col-12 d-flex justify-content-between align-items-center">
+                                            <div className="side-right d-flex align-items-center">
+                                                <OffcanvasMenu isAdmin={true} />
+                                                <h3 className='fw-bold d-none d-lg-flex align-items-center'>
+                                                    <span className='username d-inline-block me-1 px-1'>
+                                                        هادی حیدری آذر
+                                                    </span>
+                                                    عزیز، خوش اومدی🖐
+                                                </h3>
+                                            </div>
+                                            <div className="side-left d-flex">
+                                                <a href="/" className='btn me-3 rounded-pill bg-main-color p-3 btn-home-page'>
+                                                    <AiFillHome className='text-white btn-home-page-icon' />
+                                                </a>
+                                                <img src='/images/profiles/default-profile.jpg' alt="My Profile" className="img-fluid" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h3 className='fw-bold d-block align-items-center d-lg-none'>
+                                        <span className='username d-inline-block me-1 ps-2 ps-sm-4'>
+                                            هادی حیدری آذر
+                                        </span>
+                                        عزیز، خوش اومدی🖐
+                                    </h3>
+                                </div>
+                                <div className="sidebar-container mt-5">
+                                    <Outlet />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -7,17 +7,19 @@ import Courses from "./Pages/Courses/Courses";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import UserPanel from "./Pages/UserPanel/Index";
 
 const routes = [
     { path: '', element: <Home /> },
     { path: '/courses', element: <Courses /> },
     { path: '/articles', element: <Articles /> },
-    {path: '/article/:articleName', element: <Article />},
+    { path: '/article/:articleName', element: <Article /> },
     { path: '/course/:courseName', element: <CourseInfo /> },
-    {path: '/category/:categoryName', element: <Category />},
-    {path: '/contact-us', element: <ContactUs />},
+    { path: '/category/:categoryName', element: <Category /> },
+    { path: '/contact-us', element: <ContactUs /> },
     { path: '/login', element: <Login /> },
     { path: '/register', element: <Register /> },
+    { path: '/my-account', element: <UserPanel /> }
 ]
 
 export default routes
